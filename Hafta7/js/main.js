ba93d427df21669dbc0899b48fba3a7f6f6a56ea
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-29 20:18:04 +0300

    Kamera bölümü yapıldı

---
+++ Hafta7/js/main.js
@@ -0,0 +1,14 @@
+(function (){
+    'use strict';
+
+    var camera = new Swiper('#camera .swiper', {
+        speed: 600,
+        spaceBetween: 12,
+        navigation: {
+            nextEl : '.swiper-button-next',
+            prevEl : '.swiper-button-prev',
+        }
+
+
+    })
+}) ();
